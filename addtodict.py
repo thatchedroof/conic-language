4525010a0aa6e907e2f5e62b52c256c8724e5f2d
thatchedroof <tnert.wire100@gmail.com>
2021-08-31 18:44:06 -0700

    Add words

--- addtodict.py
+++ addtodict.py
@@ -20,20 +20,27 @@ coneme_to_rep = {
 }
 
 columns = ['english','synonyms']
+#print('\n'.join([coneme_to_rep[x] for x in 'FDEN']))
 
 def ask_for_entry():
     english = input('Please give an english word')
     synonyms = input('Please give any synonyms')
     conic = secrets.choice(conemes) + secrets.choice(conemes) + secrets.choice(conemes) + secrets.choice(conemes)
-    if conic in list(cdict.index) or any([x + x in conic for x in conemes]):
-        conic = secrets.choice(conemes) + secrets.choice(conemes) + secrets.choice(conemes)
+    while conic in list(cdict.index) or any([x + x in conic for x in conemes]):
+        conic = secrets.choice(conemes) + secrets.choice(conemes) + secrets.choice(conemes) + secrets.choice(conemes)
     return pd.DataFrame([[english, synonyms]], columns=columns, index=[conic])
 
 if __name__ == '__main__':
-    cdict = pd.read_csv('conicdictionary.csv',
+    again = True
+    while again == True:
+        cdict = pd.read_csv('conicdictionary.csv',
                         index_col='conic')
-    new_entry = ask_for_entry()
-    print('\n'.join([coneme_to_rep[x] for x in 'FDEN']))
-    cdict.append(new_entry).to_csv('conicdictionary.csv')
+        new_entry = ask_for_entry()
+        new_cdict = cdict.append(new_entry)
+        new_cdict.index.name = 'conic'
+        new_cdict.to_csv('conicdictionary.csv')
+        again = True if input('Again?') in {'', 'yes', 'Yes', 'y', 'Y'} else False
+
+# %%
 
 # %%
